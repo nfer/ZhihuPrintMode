a5efa0b548bf8721f7e2bfa86dcd520b9f668b62
Nfer zhuang <nfer.zhuang@gmail.com>
2016-02-22 16:32:31 +0800

    use context menu to enter print mode

---
+++ js/background.js
@@ -0,0 +1,18 @@
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if ( info.menuItemId == "zhihuprint-context-page" ) {
+    chrome.tabs.query({
+        "active": true,
+        "currentWindow": true
+    }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+            "functiontoInvoke": "hideElement"
+        });
+    });
+  }
+});
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({"title": "知乎打印模式", "contexts":["page"], "id": "zhihuprint-context-page"});
+});
--- js/content_action.js
+++ js/content_action.js
@@ -1,21 +1,26 @@
-$(document).ready(function(){
-  console.log("say hello!");
 
-  // hide topbar
-  $(".zu-top").hide();
-  $(".zu-global-notify").hide();
+function hideElement () {
+    // hide topbar
+    $(".zu-top").hide();
+    $(".zu-global-notify").hide();
 
-  // hide slidebar
-  $(".zu-main-sidebar").hide();
+    // hide slidebar
+    $(".zu-main-sidebar").hide();
 
-  // hide votebar
-  $(".zm-votebar").hide();
+    // hide votebar
+    $(".zm-votebar").hide();
 
-  // hide comment
-  $(".zm-item-comment-el").hide();
+    // hide comment
+    $(".zm-item-comment-el").hide();
 
-  // hide more answer
-  $(".more-awesome").hide();
-  $(".awesome-answer-list").hide();
-  $(".zh-answers-title").hide();
+    // hide more answer
+    $(".more-awesome").hide();
+    $(".awesome-answer-list").hide();
+    $(".zh-answers-title").hide();
+}
+
+chrome.extension.onMessage.addListener(function (message, sender, callback) {
+    if (message.functiontoInvoke == "hideElement") {
+        hideElement();
+    }
 });
