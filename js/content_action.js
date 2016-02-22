5a0844d65e0606e81945373d4f365a2771a3b92f
Nfer zhuang <nfer.zhuang@gmail.com>
2016-02-22 15:18:15 +0800

    remove no-need-print elements

--- js/content_action.js
+++ js/content_action.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
   console.log("say hello!");
+
+  // hide topbar
+  $(".zu-top").hide();
+  $(".zu-global-notify").hide();
+
+  // hide slidebar
+  $(".zu-main-sidebar").hide();
+
+  // hide votebar
+  $(".zm-votebar").hide();
+
+  // hide comment
+  $(".zm-item-comment-el").hide();
+
+  // hide more answer
+  $(".more-awesome").hide();
+  $(".awesome-answer-list").hide();
+  $(".zh-answers-title").hide();
 });
