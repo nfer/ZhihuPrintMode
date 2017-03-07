75765481ec36d9c073a4488da4db4682fb7ee281
Nfer Zhuang <nfer.zhuang@gmail.com>
2017-03-07 22:48:16 +0800

    add more rules to remove elements

--- js/content_action.js
+++ js/content_action.js
@@ -1,5 +1,9 @@
 
 function hideWWWElement () {
+    // reset body padding-top
+    $("body").css("padding-top", "0")
+    $("body").css("padding-bottom", "0")
+
     // remove topbar
     $(".zu-top").remove();
     $(".zu-global-notify").remove();
@@ -9,6 +13,11 @@ function hideWWWElement () {
 
     // remove votebar
     $(".zm-votebar").remove();
+    $(".zm-item-link-avatar").remove();
+    $(".zm-item-vote-info").remove();
+
+    // remove zm-meta-panel except answer-date-link
+    $('.zm-meta-panel *:gt(0)').remove();
 
     // remove comment
     $(".zm-item-comment-el").remove();
@@ -17,6 +26,15 @@ function hideWWWElement () {
     $(".more-awesome").remove();
     $(".awesome-answer-list").remove();
     $(".zh-answers-title").remove();
+
+    $(".zu-button-more").remove()
+
+    $(".zh-question-answer-form-wrap").remove()
+    $(".zh-footer").remove()
+    $(".FeedbackButton-button-3waL").remove()
+
+    // remove not the first answer
+    $('div.zm-item-answer:gt(0)').remove();
 
     // replace multi break line to one break line
     var $zm_editable_content = $("div.zm-item-answer div.zm-item-rich-text .zm-editable-content");
